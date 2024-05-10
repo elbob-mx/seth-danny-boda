5f188bc8f18ebd618ec6c5d5dc4a3f071f3f9240
Roberto Gutierrez <rob7th@gmail.com>
2024-05-10 17:01:32 -0600

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./*.{html,js}"],
+    content: ["*.{html,js}"],
     theme: {
-        extend: {},
+        screens: {
+            small: "0px",
+            mobile: "375px",
+            mobileOldHorSmall: "419px",
+            mobileHorSmall: "567px",
+            mobileHor: "640px",
+            tablet: "767px",
+            bigHor: "811px",
+            vertIpadMini: "820px",
+            bigPhoneHor: "844px",
+            laptop: "976px",
+            ipad: "1023px",
+            hd: "1365px",
+            fhd: "1440px",
+        },
+        extend: {
+            fontFamily: {
+                josefinSans: "Josefin Sans, sans-serif",
+                josefinSlab: "Josefin Slab, serif",
+                belle: "La Belle Aurore, cursive",
+            },
+            colors: {
+                offWhite: "#f2f2f2",
+                offBlack: "#181818",
+                greige: "#e6dcd3",
+                midGreige: "#b4a793",
+                btnHover: "#bda18c",
+                btnNormal: "#5d4e41 ",
+            },
+        },
     },
     plugins: [],
 };
